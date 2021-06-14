600baf1dff47a92620636f753d9b777af4b64b9b
firesoflife <bryangoertz@gmail.com>
2021-06-14 13:46:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [image, setImage] = useState('');
+	const clientId = 'SJoYwh9wRf56F4ikb9bYYtCNHDrxtg8pKP57HXgtY64';
+	const [result, setResult] = useState([]);
+
+	const handleChange = e => {
+		setImage(e.target.value);
+	};
+
+	const handleSubmit = () => {
+		const url =
+			'https://api.unsplash.com/search/photos?page=1&query=' +
+			image +
+			'&client_id=' +
+			clientId;
+
+		axios.get(url).then(response => {
+			setResult(response.data.results);
+		});
+	};
+
+	return (
+		<div className='App'>
+			<div className='heading'>
+				<h1>React Image Search Using Unsplash API.</h1>
+			</div>
+			<div className='input'>
+				<input
+					onChange={handleChange}
+					type='text'
+					name='image'
+					placeholder='Search for images'
+				/>
+			</div>
+			<button onClick={handleSubmit} type='submit'>
+				Search
+			</button>
+			<div className='result'>
+				{result.map(image => (
+					<Fragment>
+						<div className='card'>
+							<img src={image.urls.thumb} alt='' />
+							<p className='username'>Photo by {image.user.name} </p>
+							<p className='like'>{image.likes} </p>
+						</div>
+					</Fragment>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
